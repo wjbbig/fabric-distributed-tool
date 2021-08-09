515cd890a4f77ac53532ca94482c3e0b6f0ab3f6
wjbbig <JiabinWu1998@outlook.com>
2021-08-09 18:21:19 +0800

    update network config

--- network/network.go
+++ network/network.go
@@ -1,26 +1,45 @@
 package network
 
 type NetworkConfig struct {
-	Name      string
-	Channels  map[string]*Channel
-	Nodes     map[string]*Node
-	Chaincode map[string]*Chaincode
+	Name       string                `yaml:"name,omitempty"`
+	Channels   map[string]*Channel   `yaml:"channels,omitempty"`
+	Nodes      map[string]*Node      `yaml:"nodes,omitempty"`
+	Chaincodes map[string]*Chaincode `yaml:"chaincodes,omitempty"`
 }
 
 type Node struct {
-	Name string
-	Port string
-	Type string
+	Name     string `yaml:"name,omitempty"`
+	NodePort int    `json:"node_port,omitempty"`
+	Type     string `yaml:"type,omitempty"`
+	Username string `yaml:"username,omitempty"`
+	Password string `yaml:"password,omitempty"`
+	Host     string `yaml:"host,omitempty"`
+	SSHPort  int    `yaml:"ssh_port,omitempty"`
+	Couch    bool   `yaml:"couch,omitempty"`
 }
 
 type Channel struct {
-	Nodes     []string
-	Chaincode []string
+	Nodes      []string `yaml:"nodes,omitempty"`
+	Chaincodes []string `yaml:"chaincodes,omitempty"`
 }
 
 type Chaincode struct {
-	Name      string
-	Path      string
-	Version   string
-	InitParam string
+	Path      string `yaml:"path,omitempty"`
+	Version   string `yaml:"version,omitempty"`
+	InitParam string `yaml:"init_param,omitempty"`
+}
+
+func GenerateNetworkConfig(fileDir, networkName, channelId, ccId, ccPath, ccVersion, ccInitParam string, peerUrls, ordererUrls []string) error {
+	var network NetworkConfig
+
+	network.Name = networkName
+	chaincode := &Chaincode{
+		Path:      ccPath,
+		Version:   ccVersion,
+		InitParam: ccInitParam,
+	}
+	network.Chaincodes = map[string]*Chaincode{
+		ccId: chaincode,
+	}
+	return nil
 }
